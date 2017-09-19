7b39afd35e979fa264855b4a9cae6c3c0649fda7
Xu Jing <jingxu086@gmail.com>
2017-09-19 00:32:43 -0400

    add The game is running successfully

--- js/tic-tac-toe.js
+++ js/tic-tac-toe.js
@@ -5,26 +5,57 @@ var SIZE = 3,
   score,
   moves;
 
+function check(cell){
+  var memberOf = cell.id.split(/\s+/);
+  for (var i = 0; i < memberOf.length; i++) {
+    score[turn][memberOf[i]] += 1;
+    if (score[turn][memberOf[i]] == SIZE) {
+      return true;
+    }
+  }
+  return false
+}
+
 function play(element) {
   if (element.innerHTML !== EMPTY) {
     return;
   }
   element.innerHTML = turn;
   moves += 1;
-
-  if (moves === 9){
+  if (check(element)){
+    alert(turn + ' Win!');
+    newGame();
+  } else if (moves === 9){
     alert('Draw');
     newGame();
+  } else {
+    // switch
+    turn = turn === 'X' ? 'O' : 'X';
   }
-
-  // switch
-  turn = turn === 'X' ? 'O' : 'X';
 }
 
 function newGame(){
   score = {
-    'X': 0,
-    'O': 0
+    'X': {
+      'col0': 0,
+      'col1': 0,
+      'col2': 0,
+      'row0': 0,
+      'row1': 0,
+      'row2': 0,
+      'diagonal0': 0,
+      'diagonal1': 0,
+    },
+    'O': {
+      'col0': 0,
+      'col1': 0,
+      'col2': 0,
+      'row0': 0,
+      'row1': 0,
+      'row2': 0,
+      'diagonal0': 0,
+      'diagonal1': 0,
+    },
   };
   moves = 0;
   turn = 'X';
